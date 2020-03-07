70e681bd416d6c844b6a96ff2fc9c3ecfce84910
Simon Whitaker <sw@netcetera.org>
2020-03-07 10:41:24 +0000

    Use absolute path to quiz.jq

--- quiz.sh
+++ quiz.sh
@@ -5,4 +5,4 @@ set -o errexit
 set -o pipefail
 set -o noclobber
 
-curl -sSf https://saturday-quiz.herokuapp.com/api/quiz | jq -r -f quiz.jq
+curl -sSf https://saturday-quiz.herokuapp.com/api/quiz | jq -r -f $(dirname $0)/quiz.jq
